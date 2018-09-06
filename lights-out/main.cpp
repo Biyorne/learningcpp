fa9771d7d62a35e75a4dabab45329903007918af
Biyorne <biyorne@gmail.com>
2018-09-06 16:59:17 -0600

    Dependency is working.

--- lights-out/main.cpp
+++ lights-out/main.cpp
@@ -1,6 +1,29 @@
-#include <cstdlib>
+#include <SFML/Graphics.hpp>
 
-int main(void)
+int main()
 {
+	
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Lights Out");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{ 
+				window.close();
+			}
+			if (event.type == sf::Event::KeyPressed)
+			{
+				window.close();
+			}
+		}
+
+		window.clear();
+		//window.draw(shape);
+		window.display();
+	}
+
 	return EXIT_SUCCESS;
 }
