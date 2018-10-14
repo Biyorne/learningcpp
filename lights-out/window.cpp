84a1fc023295e970302663a0b52852608231616e
Biyorne <biyorne@gmail.com>
2018-10-14 15:12:35 -0600

    Fixed build problem with window

--- lights-out/window.cpp
+++ lights-out/window.cpp
@@ -1,48 +1,41 @@
-#include "game-board.hpp"
 #include "window.hpp"
 
 namespace lightsout
 {
-    class Window
+
+    Window::Window(
+        const std::string & TITLE,
+        const unsigned int WIDTH,
+        const unsigned int HEIGHT,
+        const sf::Color & COLOR)
+        : m_renderWindow(sf::VideoMode(WIDTH, HEIGHT), TITLE)
+        , m_backgroundColor(COLOR)
+    {}
+
+    void Window::handleEvents(GameBoard & gameBoard)
     {
-    public:
-        Window(
-            const std::string & TITLE,
-            const unsigned int WIDTH,
-            const unsigned int HEIGHT,
-            const sf::Color & COLOR)
-            : m_renderWindow(sf::VideoMode(WIDTH, HEIGHT), TITLE)
-            , m_backgroundColor(COLOR)
-        {}
-
-        void handleEvents(GameBoard & gameBoard)
+        sf::Event event;
+        while (m_renderWindow.pollEvent(event))
         {
-            sf::Event event;
-            while (m_renderWindow.pollEvent(event))
+            if (event.type == sf::Event::Closed)
             {
-                if (event.type == sf::Event::Closed)
-                {
-                    m_renderWindow.close();
-                }
-
-                gameBoard.eventHandler(event);
-            }
-        }
-
-        void draw(const GameBoard & GAME_BOARD)
-        {
-            m_renderWindow.clear(m_backgroundColor);
-
-            for (const lightsout::Cell & CELL : GAME_BOARD.cells())
-            {
-                m_renderWindow.draw(CELL.rectangle());
+                m_renderWindow.close();
             }
 
-            m_renderWindow.display();
+            gameBoard.eventHandler(event);
+        }
+    }
+
+    void Window::draw(const GameBoard & GAME_BOARD)
+    {
+        m_renderWindow.clear(m_backgroundColor);
+
+        for (const lightsout::Cell & CELL : GAME_BOARD.cells())
+        {
+            m_renderWindow.draw(CELL.rectangle());
         }
 
-    private:
-        sf::RenderWindow m_renderWindow;
-        sf::Color m_backgroundColor;
-    };
+        m_renderWindow.display();
+    }
+
 } // namespace lightsout
--- lights-out/window.hpp
+++ lights-out/window.hpp
@@ -1,6 +1,8 @@
 #ifndef LIGHTS_OUT_WINDOW_UTIL_HPP_INCLUDED
 #define LIGHTS_OUT_WINDOW_UTIL_HPP_INCLUDED
 
+#include "game-board.hpp"
+
 #include <SFML/Graphics.hpp>
 
 namespace lightsout
@@ -8,6 +10,12 @@ namespace lightsout
     class Window
     {
     public:
+        Window(
+            const std::string & TITLE,
+            const unsigned int WIDTH,
+            const unsigned int HEIGHT,
+            const sf::Color & COLOR);
+
         sf::Vector2f size() const { return sf::Vector2f(m_renderWindow.getSize()); }
 
         bool isOpen() const { return m_renderWindow.isOpen(); }
