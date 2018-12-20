ab87887e210cf8b4010377e9b6faba33d157a900
Biyorne <biyorne@gmail.com>
2018-12-19 20:30:37 -0700

    Moved the GameState enum to its own file.

---
+++ lights-out/game-state-enum.hpp
@@ -0,0 +1,15 @@
+#ifndef LIGHTS_OUT_GAME_STATE_ENUM_HPP_INCLUDED
+#define LIGHTS_OUT_GAME_STATE_ENUM_HPP_INCLUDED
+
+namespace lightsout
+{
+    enum class GameState
+    {
+        Help = 0,
+        Play,
+        Win,
+        Lose
+    };
+}
+
+#endif // LIGHTS_OUT_GAME_STATE_ENUM_HPP_INCLUDED
