8b3495d7a613f52bf75980562ba2af6506532e95
Biyorne <biyorne@gmail.com>
2019-05-01 20:46:11 -0600

    Finished race simulator.

--- race-math/main.cpp
+++ race-math/main.cpp
@@ -1,6 +1,19 @@
 #include <SFML/Graphics.hpp>
 
+#include <cmath>
 #include <iostream>
+
+float calcPos(
+    const float startPos, const float velocity, const float timeSec, const float acceleration)
+{
+    return (startPos + (velocity * timeSec) + (acceleration * timeSec * timeSec));
+}
+
+float calcOvertakeTimeSec(
+    const float slowStartPos, const float fastStartPos, const float slowAcc, const float fastAcc)
+{
+    return std::sqrt((fastStartPos - slowStartPos) / (slowAcc - fastAcc));
+}
 
 int main()
 {
@@ -17,10 +30,25 @@ int main()
 
     const sf::Vector2f WINDOW_SIZE(window.getSize());
 
-    slowCarSprite.setPosition(
+    const float slowCarAcc(10.0f);
+    const float fastCarAcc(slowCarAcc * 2.0f);
+
+    const sf::Vector2f slowCarStartPos(
         (WINDOW_SIZE.x * 0.25f),
         (fastCarSprite.getGlobalBounds().top + fastCarSprite.getGlobalBounds().height));
 
+    const sf::Vector2f fastCarStartPos;
+
+    slowCarSprite.setPosition(slowCarStartPos);
+
+    const float overtakePredictedTimeSec(
+        calcOvertakeTimeSec(slowCarStartPos.x, fastCarStartPos.x, slowCarAcc, fastCarAcc));
+
+    std::cout << "Prediction = " << overtakePredictedTimeSec << std::endl;
+
+    float overtakeActualTimeSec(0.0f);
+
+    sf::Clock raceClock;
     while (window.isOpen())
     {
         sf::Event event;
@@ -38,21 +66,24 @@ int main()
         window.draw(fastCarSprite);
         window.display();
 
-        slowCarSprite.move(sf::Vector2f(1.0f, 0.0f));
-        fastCarSprite.move(sf::Vector2f(1.0f, 0.0f));
+        const float elapsedTimeSec(raceClock.getElapsedTime().asSeconds());
 
-        if (slowCarSprite.getPosition().x > WINDOW_SIZE.x)
-        {
-            slowCarSprite.setPosition(
-                -slowCarSprite.getGlobalBounds().width, slowCarSprite.getPosition().y);
-        }
+        slowCarSprite.setPosition(
+            calcPos(slowCarStartPos.x, 0.0f, elapsedTimeSec, slowCarAcc), 0.0f);
 
-        if (fastCarSprite.getPosition().x > WINDOW_SIZE.x)
+        fastCarSprite.setPosition(
+            calcPos(fastCarStartPos.x, 0.0f, elapsedTimeSec, fastCarAcc), 0.0f);
+
+        if (fastCarSprite.getPosition().x > slowCarSprite.getPosition().x)
         {
-            fastCarSprite.setPosition(
-                -fastCarSprite.getGlobalBounds().width, fastCarSprite.getPosition().y);
+            overtakeActualTimeSec = elapsedTimeSec;
+            std::cout << "Actual = " << overtakeActualTimeSec << std::endl;
+            window.close();
         }
     }
+
+    std::cout << "We were off by " << std::fixed
+              << std::abs(overtakeActualTimeSec - overtakePredictedTimeSec) << "." << std::endl;
 
     return 0;
 }
