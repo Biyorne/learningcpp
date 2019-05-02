c4e87a2782542b54efca59945bc0aa8e0b110f99
Biyorne <biyorne@gmail.com>
2019-05-01 18:35:31 -0600

    Got both cars racing.

--- race-math/main.cpp
+++ race-math/main.cpp
@@ -4,7 +4,6 @@
 
 int main()
 {
-
     sf::Texture slowCarTexture;
     sf::Texture fastCarTexture;
 
@@ -15,6 +14,12 @@ int main()
     sf::Sprite fastCarSprite(fastCarTexture);
 
     sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Race Math", sf::Style::Fullscreen);
+
+    const sf::Vector2f WINDOW_SIZE(window.getSize());
+
+    slowCarSprite.setPosition(
+        (WINDOW_SIZE.x * 0.25f),
+        (fastCarSprite.getGlobalBounds().top + fastCarSprite.getGlobalBounds().height));
 
     while (window.isOpen())
     {
@@ -32,6 +37,21 @@ int main()
         window.draw(slowCarSprite);
         window.draw(fastCarSprite);
         window.display();
+
+        slowCarSprite.move(sf::Vector2f(1.0f, 0.0f));
+        fastCarSprite.move(sf::Vector2f(1.0f, 0.0f));
+
+        if (slowCarSprite.getPosition().x > WINDOW_SIZE.x)
+        {
+            slowCarSprite.setPosition(
+                -slowCarSprite.getGlobalBounds().width, slowCarSprite.getPosition().y);
+        }
+
+        if (fastCarSprite.getPosition().x > WINDOW_SIZE.x)
+        {
+            fastCarSprite.setPosition(
+                -fastCarSprite.getGlobalBounds().width, fastCarSprite.getPosition().y);
+        }
     }
 
     return 0;
