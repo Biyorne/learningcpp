a34b07a7e4dbc13f53492956b2cc7d95b7ebce6a
Biyorne <biyorne@gmail.com>
2019-05-20 17:51:02 -0600

    Progress on score drawing

--- meth-heads/main.cpp
+++ meth-heads/main.cpp
@@ -9,9 +9,9 @@
 //    Number of cell spaces
 //    Number of loot items on screen at a time
 
-struct BoardView
+struct DisplayConstants
 {
-    BoardView(const sf::Vector2u & windowSize)
+    DisplayConstants(const sf::Vector2u & windowSize)
         : pad_ratio(0.1f)
         , window_size(windowSize)
         , score_region(0.0f, 0.0f, (0.2f * window_size.x), window_size.y)
@@ -20,13 +20,19 @@ struct BoardView
               score_region.top,
               (window_size.x - score_region.width),
               score_region.height)
-        , background_color { 32, 32, 32 }
-        , foreground_color { 220, 220, 220 }
+        , cell_background_color { 32, 32, 32 }
+        , cell_line_color { 220, 220, 220 }
+        , lazy_color { 100, 100, 255 }
+        , greedy_color { 100, 255, 100 }
+        , prize_color { 255, 255, 100 }
         , column_count { 20 }
         , row_count { column_count }
         , line_thickness_ratio(1.0f / 600.0f)
         , line_thickness(line_thickness_ratio * window_size.y)
         , rectangles()
+        , cell_dimm(window_size.y / static_cast<float>(std::max(column_count, row_count)))
+        , cell_size(cell_dimm, cell_dimm)
+        , score_rectangle_width(score_region.width * 0.5f)
     {
         makeGrid();
     }
@@ -35,21 +41,27 @@ struct BoardView
     sf::Vector2f window_size;
     sf::FloatRect score_region;
     sf::FloatRect board_region;
-    sf::Color background_color;
-    sf::Color foreground_color;
+    sf::Color cell_background_color;
+    sf::Color cell_line_color;
+    sf::Color lazy_color;
+    sf::Color greedy_color;
+    sf::Color prize_color;
     std::size_t column_count;
     std::size_t row_count;
     float line_thickness_ratio;
     float line_thickness;
     std::vector<sf::RectangleShape> rectangles;
+    float cell_dimm;
+    sf::Vector2f cell_size;
+    float score_rectangle_width;
 
 private:
     inline const sf::RectangleShape
-        makeRectangleShape(const sf::Vector2f & pos, const sf::Vector2f & size) const
+        makeGridRectangleShape(const sf::Vector2f & pos, const sf::Vector2f & size) const
     {
         sf::RectangleShape temporary(size);
-        temporary.setFillColor(background_color);
-        temporary.setOutlineColor(foreground_color);
+        temporary.setFillColor(cell_background_color);
+        temporary.setOutlineColor(cell_line_color);
         temporary.setOutlineThickness(line_thickness);
         temporary.setPosition(pos);
         return temporary;
@@ -57,22 +69,26 @@ private:
 
     inline void makeGrid()
     {
-        const float cellDimm(window_size.y / static_cast<float>(std::max(column_count, row_count)));
-        const sf::Vector2f cellSize(cellDimm, cellDimm);
-
         const sf::Vector2f boardPos(board_region.left, board_region.top);
+
+        const sf::Vector2f gridTotalSize(
+            (cell_size.x * static_cast<float>(column_count)),
+            (cell_size.y * static_cast<float>(row_count)));
+
+        const float centerOffsetHoriz((board_region.width - gridTotalSize.x) * 0.5f);
+        const float centerOffsetVert((board_region.height - gridTotalSize.y) * 0.5f);
+        const sf::Vector2f centerOffset(centerOffsetHoriz, centerOffsetVert);
 
         for (std::size_t c(0); c < column_count; ++c)
         {
             for (std::size_t r(0); r < row_count; ++r)
             {
-                const sf::Vector2f cellPos((c * cellSize.x), (r * cellSize.y));
+                const sf::Vector2f cellPos((c * cell_size.x), (r * cell_size.y));
 
                 float left(board_region.width);
 
-                const sf::Vector2f centerOffset(, );
-
-                rectangles.push_back(makeRectangleShape((boardPos + cellPos), cellSize));
+                rectangles.push_back(
+                    makeGridRectangleShape((boardPos + centerOffset + cellPos), cell_size));
             }
         }
     }
@@ -80,7 +96,6 @@ private:
 
 int main()
 {
-
     sf::Texture lazyMethHeadTexture;
     sf::Texture greedyMethHeadTexture;
     sf::Texture lootCheapTexture;
@@ -96,28 +111,17 @@ int main()
 
     sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Meth Heads", sf::Style::Fullscreen);
 
-    const BoardView boardView(window.getSize());
+    const DisplayConstants displayConstants(window.getSize());
 
-    // sf::RectangleShape rectIWant = boardView.makeRectangleShape(sf::Vector2f(100.0f, 100.0f));
+    // Score Column Drawing Here
+    std::size_t lazyScore(1);
+    std::size_t greedyScore(1);
 
-    const sf::Vector2f lazyMethHeadPos(
-        (boardView.window_size.x * 0.25f),
-        (greedyMethHeadSprite.getGlobalBounds().top
-         + greedyMethHeadSprite.getGlobalBounds().height));
+    sf::RectangleShape lazyScoreRectangle;
+    lazyScoreRectangle.setFillColor(displayConstants.lazy_color);
 
-    const sf::Vector2f lootCheapPos(
-        0,
-        (greedyMethHeadSprite.getGlobalBounds().top
-         + greedyMethHeadSprite.getGlobalBounds().height));
-
-    const sf::Vector2f lootValuablePos((boardView.window_size.x * 0.25f), 0);
-
-    const sf::Vector2f greedyMethHeadStartPos;
-
-    using Pos_t = sf::Vector2i;
-
-    lazyMethHeadSprite.setPosition(lazyMethHeadPos);
-    lootCheapSprite.setPosition(lootCheapPos);
+    sf::RectangleShape greedyScoreRectangle;
+    greedyScoreRectangle.setFillColor(displayConstants.greedy_color);
 
     while (window.isOpen())
     {
@@ -130,11 +134,35 @@ int main()
             }
         }
 
+        // Setup Score Rectangles
+
+        lazyScore += 2;
+        greedyScore += 3;
+
+        lazyScoreRectangle.setSize(
+            sf::Vector2f(displayConstants.score_rectangle_width, static_cast<float>(lazyScore)));
+
+        greedyScoreRectangle.setSize(
+            sf::Vector2f(displayConstants.score_rectangle_width, static_cast<float>(greedyScore)));
+
+        float greedyHeightRatio(greedyScoreRectangle.getSize().y / lazyScoreRectangle.getSize().y);
+
+        lazyScoreRectangle.setPosition(
+            displayConstants.score_region.left,
+            (displayConstants.score_region.height - lazyScoreRectangle.getSize().y));
+
+        greedyScoreRectangle.setPosition(
+            (displayConstants.score_region.left + displayConstants.score_rectangle_width),
+            (displayConstants.score_region.height - greedyScoreRectangle.getSize().y));
+
         window.clear();
-        for (const auto & rectangle : boardView.rectangles)
+        for (const auto & rectangle : displayConstants.rectangles)
         {
             window.draw(rectangle);
         }
+
+        window.draw(lazyScoreRectangle);
+        window.draw(greedyScoreRectangle);
 
         // window.draw(lazyMethHeadSprite);
         // window.draw(greedyMethHeadSprite);
