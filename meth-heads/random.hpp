ca10f954eab26fc6bf24cd654f70193fab2794b1
Ziesche Til Newman <tilnewman@protonmail.com>
2019-12-15 12:36:28 -0700

    fixed the bugs with the random cache

--- meth-heads/random.hpp
+++ meth-heads/random.hpp
@@ -15,13 +15,10 @@
 
 namespace methhead
 {
-    // Remember:
-    //  It's about 10-100x faster to re-generate random numbers than to shuffle containers.
-    template <typename Ratio_t = float, std::size_t CacheSize = 0>
+    // Remember:  It's 10-100x faster to re-generate random numbers than to shuffle containers.
+
     class RandomMt
     {
-        static_assert(std::is_floating_point_v<Ratio_t>);
-
       public:
         RandomMt()
             : RandomMt(std::random_device{}())
@@ -29,25 +26,17 @@ namespace methhead
 
         explicit RandomMt(const std::random_device::result_type seed)
             : m_engine()
-            , m_cache()
-            , m_cacheIndex(0)
         {
             std::seed_seq seedSequence{ seed };
             m_engine.seed(seedSequence);
 
-            std::cout << "Random number generator <" << NAMEOF(m_engine)
-                      << "> (Mersenne Twister)'s seed: " << seed << std::endl;
+            std::cout << "RandomMt<" << NAMEOF(m_engine) << "> Seed: " << seed << std::endl;
 
             // Warm-up-skipping is good standard practice when working with PRNGs, but the Mersenne
             // Twister is notoriously predictable in the beginning.  This is especially true when
             // you don't provide a good (full sized) seed, which I am not because I want the ease of
             // troubleshooting games. Anything from thousands to hundreds-thousands works fine here.
             m_engine.discard(123456);
-
-            if constexpr (isCacheEnabled())
-            {
-                setupCache();
-            }
         }
 
         template <typename T>
@@ -56,19 +45,6 @@ namespace methhead
             if (to < from)
             {
                 return fromTo(to, from);
-            }
-
-            if constexpr (isCacheEnabled())
-            {
-                Ratio_t offset{ incrementAndGetCachedRatio() *
-                                (static_cast<Ratio_t>(to) - static_cast<Ratio_t>(from)) };
-
-                if constexpr (std::is_integral_v<T>)
-                {
-                    offset = std::round(offset);
-                }
-
-                return static_cast<T>(static_cast<Ratio_t>(from) + offset);
             }
 
             if constexpr (std::is_floating_point_v<T>)
@@ -97,37 +73,30 @@ namespace methhead
             return fromTo(T(0), to);
         }
 
-        inline Ratio_t ratio() const
+        template <typename T = float>
+        T ratio() const
         {
-            if constexpr (isCacheEnabled())
-            {
-                return incrementAndGetCachedRatio();
-            }
-            else
-            {
-                return zeroTo(Ratio_t(1));
-            }
+            return zeroTo(T(1));
         }
 
         template <typename T>
-        std::enable_if_t<std::is_unsigned_v<T>, std::size_t> index(const T size) const
+        std::size_t index(const T size) const
         {
             if (size <= 1)
             {
                 return 0;
             }
 
-            const T randomValue{ zeroTo(size - T(1)) };
-            return static_cast<std::size_t>(randomValue);
+            return static_cast<std::size_t>(zeroTo(size - T(1)));
         }
 
         template <typename T>
-        std::enable_if_t<!std::is_unsigned_v<T>, std::size_t> index(const T & container) const
+        std::size_t indexFrom(const T & container) const
         {
             if (container.size() == 0)
             {
                 std::string message;
-                message += "Random::index(container<";
+                message += "RandomMt::index(container<";
                 message += nameof::nameof_full_type<T>();
                 message += ">) but that container.size() == 0!";
 
@@ -137,17 +106,7 @@ namespace methhead
             return index(container.size());
         }
 
-        inline bool boolean() const
-        {
-            if constexpr (isCacheEnabled())
-            {
-                return (incrementAndGetCachedRatio() < Ratio_t(0.5));
-            }
-            else
-            {
-                return (zeroTo(1) == 0);
-            }
-        }
+        inline bool boolean() const { return (zeroTo(1) == 0); }
 
         template <typename Iter_t>
         auto & from(Iter_t first, const Iter_t last) const
@@ -155,7 +114,7 @@ namespace methhead
             if (last == first)
             {
                 std::string message;
-                message += "Random::from(<";
+                message += "RandomMt::from(<";
                 message += nameof::nameof_full_type<Iter_t>();
                 message += ">) but the container was empty!";
 
@@ -191,44 +150,155 @@ namespace methhead
             shuffle(std::begin(container), std::end(container));
         }
 
-        static constexpr bool isCacheEnabled() noexcept { return (CacheSize > 0); }
-
-        static constexpr std::size_t cacheSize() noexcept { return CacheSize; }
-
       private:
-        void setupCache()
-        {
-            std::uniform_real_distribution<Ratio_t> dist(
-                Ratio_t(0), std::nextafter(Ratio_t(1), Ratio_t(2)));
+        mutable std::mt19937 m_engine;
+    };
 
+    //
+
+    template <typename Number_t = float, std::size_t CacheSize = 10000>
+    class RandomMtCache
+    {
+        static_assert(std::is_floating_point_v<Number_t>);
+        static_assert(CacheSize > 0);
+
+      public:
+        using value_t = Number_t;
+        static constexpr std::size_t cacheSize = CacheSize;
+
+        RandomMtCache()
+            : RandomMtCache(std::random_device{}())
+        {}
+
+        explicit RandomMtCache(const std::random_device::result_type seed)
+            : m_engine(seed)
+            , m_cache()
+            , m_cacheIndex(0)
+        {
             m_cache.resize(CacheSize);
 
-            for (Ratio_t & number : m_cache)
+            for (Number_t & number : m_cache)
             {
-                number = dist(m_engine);
+                number = m_engine.ratio<Number_t>();
             }
         }
 
-        constexpr Ratio_t incrementAndGetCachedRatio() const noexcept
+        template <typename T>
+        T fromTo(const T from, const T to) const
+        {
+            Number_t offset{ incrementAndGetCachedRatio() *
+                             (static_cast<Number_t>(to) - static_cast<Number_t>(from)) };
+
+            if constexpr (std::is_integral_v<T>)
+            {
+                offset = std::roundf(offset);
+            }
+
+            return static_cast<T>(static_cast<Number_t>(from) + offset);
+        }
+
+        template <typename T>
+        T zeroTo(const T to) const
+        {
+            return fromTo(T(0), to);
+        }
+
+        constexpr Number_t ratio() const noexcept { return incrementAndGetCachedRatio(); }
+
+        template <typename T>
+        std::size_t index(const T size) const
+        {
+            if (size <= 1)
+            {
+                return 0;
+            }
+
+            return static_cast<std::size_t>(zeroTo(size - T(1)));
+        }
+
+        template <typename T>
+        std::size_t indexFrom(const T & container) const
+        {
+            if (container.size() == 0)
+            {
+                std::string message;
+                message += "RandomMtCache::index(container<";
+                message += nameof::nameof_full_type<T>();
+                message += ">) but that container.size() == 0!";
+
+                throw std::runtime_error(message);
+            }
+
+            return index(container.size());
+        }
+
+        constexpr bool boolean() const noexcept
+        {
+            return (incrementAndGetCachedRatio() < Number_t(0.5));
+        }
+
+        template <typename Iter_t>
+        auto & from(Iter_t first, const Iter_t last) const
+        {
+            if (last == first)
+            {
+                std::string message;
+                message += "RandomMtCache::from(<";
+                message += nameof::nameof_full_type<Iter_t>();
+                message += ">) but the container was empty!";
+
+                throw std::runtime_error(message);
+            }
+
+            const auto offset{ zeroTo(std::distance(first, last) - 1) };
+            std::advance(first, static_cast<std::ptrdiff_t>(offset));
+            return *first;
+        }
+
+        template <typename T>
+        auto & from(T & container) const
+        {
+            return from(std::begin(container), std::end(container));
+        }
+
+        template <typename T>
+        const T & from(const std::initializer_list<T> & list) const
+        {
+            return from(std::begin(list), std::end(list));
+        }
+
+        template <typename Iter_t>
+        void shuffle(const Iter_t first, const Iter_t last) const
+        {
+            std::shuffle(first, last, m_engine);
+        }
+
+        template <typename T>
+        void shuffle(T & container) const
+        {
+            shuffle(std::begin(container), std::end(container));
+        }
+
+      private:
+        constexpr Number_t incrementAndGetCachedRatio() const noexcept
         {
             ++m_cacheIndex;
 
             if (m_cacheIndex >= CacheSize)
             {
-                m_cacheIndex = 0;
+                m_cacheIndex = m_engine.zeroTo(m_cacheIndex / 2);
             }
 
             return m_cache[m_cacheIndex];
         }
 
       private:
-        mutable std::mt19937 m_engine;
-
-        mutable std::vector<Ratio_t> m_cache;
+        mutable RandomMt m_engine;
+        mutable std::vector<Number_t> m_cache;
         mutable std::size_t m_cacheIndex;
     };
 
-    using Random = RandomMt<float, 10000>;
+    using Random = RandomMtCache<float, 10000>;
 } // namespace methhead
 
 #endif // METHHEADS_RANDOM_HPP_INCLUDED
