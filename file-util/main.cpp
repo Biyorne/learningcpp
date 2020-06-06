4fb891b202c431719ef37303db5d85dc9af4b7ef
Biyorne <biyorne@gmail.com>
2020-06-05 21:36:36 -0600

    got it able to show which filenames are dupicates, and how many.

--- file-util/main.cpp
+++ file-util/main.cpp
@@ -2,6 +2,10 @@
 #include <cstdlib>
 #include <filesystem>
 #include <iostream>
+#include <map>
+
+// All kinds of permission/access/privilages/etc errors are logged, not included, but the app keeps
+// going.
 
 // Error Handling:
 //  Start with non-tolerant, (i.e. assert/exceptions), because it won't take so much time write.
@@ -38,7 +42,7 @@ int main()
 {
     try
     {
-        printAllFilesIn("D:/");
+        printAllFilesIn("C:/");
     }
     catch (const std::exception & ex)
     {
@@ -60,6 +64,10 @@ void printAllFilesIn(const std::filesystem::path & dirPathOrig)
 
     assert(fs::exists(dirPathCann));
     assert(fs::is_directory(dirPathCann));
+
+    using Map_t = std::map<std::string, std::size_t>;
+    using MapPair_t = std::pair<std::string, std::size_t>;
+    Map_t fileNamesToCount;
 
     for (const fs::directory_entry & entry : fs::recursive_directory_iterator(
              dirPathCann, fs::directory_options::skip_permission_denied))
@@ -83,14 +91,51 @@ void printAllFilesIn(const std::filesystem::path & dirPathOrig)
             continue;
         }
 
-        ++regularFileCount;
+        fileNamesToCount[entry.path().filename().string()]++;
 
+        // temp way to make sure it is not hung with status updates
+        ++regularFileCount;
         if ((regularFileCount % regularFileCountPerStatus) == 0)
         {
-            std::cout << "#" << regularFileCount << "\n";
+            std::cout << "#" << regularFileCount << '\n';
             regularFileCountPerStatus += regularFileCount;
         }
     }
 
-    std::cout << "Found/Had access to " << regularFileCount << " regular files.\n";
+    std::cout << "Finished searching..." << std::endl;
+
+    std::vector<MapPair_t> nameCountDups;
+    nameCountDups.reserve(fileNamesToCount.size());
+
+    for (const MapPair_t & nameCountPair : fileNamesToCount)
+    {
+        if (nameCountPair.second <= 1)
+        {
+            continue;
+        }
+
+        nameCountDups.push_back(nameCountPair);
+    }
+
+    std::sort(
+        std::begin(nameCountDups),
+        std::end(nameCountDups),
+        [](const MapPair_t & left, const MapPair_t & right) {
+            return (left.second < right.second);
+        });
+
+    for (const auto & [filename, count] : nameCountDups)
+    {
+        if (count <= 2)
+        {
+            continue;
+        }
+
+        std::cout << filename << ": " << count << '\n';
+    }
+
+    std::cout << "Found " << fileNamesToCount.size() << " regular files with unique names, but "
+              << nameCountDups.size() << " were duplicates." << std::endl;
+
+    fileNamesToCount.clear();
 }
