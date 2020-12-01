15efc054335b57a77ca13c1af3dce9f8da56a371
ObsidianArrows <biyorne@gmail.com>
2020-12-01 16:28:07 -0700

    Created stage and transition structs

--- cyoa-game/main.cpp
+++ cyoa-game/main.cpp
@@ -1,21 +1,86 @@
 #include <cstdlib>
 #include <iostream>
+#include <vector>
+
+struct Transition
+{
+    Transition(const std::size_t pId, const std::string & pDesc)
+        : id { pId }
+        , description { pDesc }
+    { }
+
+    std::size_t id;
+    std::string description;
+};
+
+struct Stage
+{
+    Stage(
+        const std::size_t pId, const std::string & pDesc, const std::vector<Transition> & pChoices)
+        : id { pId }
+        , description { pDesc }
+        , choices { pChoices }
+    { }
+
+    std::size_t id;
+    std::string description;
+    std::vector<Transition> choices;
+
+    bool isValidChoice(const std::size_t choice) const
+    {
+        return ((choice <= (choices.size())) && (choice != 0));
+    }
+
+    std::size_t changeStage(const std::size_t choice) const
+    {
+        if (!isValidChoice(choice))
+        {
+            std::cout << "Invalid choice, staying here." << std::endl;
+            return id;
+        }
+
+        return choices.at(choice - 1).id;
+    }
+};
 
 int main()
 {
-    std::cout << "You are in a jungle. Do you go (l)eft or (r)ight?" << std::endl;
+    const Stage jungle { 0,
+                         "You are in a jungle. Do you go:",
+                         { Transition { 1, "left" }, Transition { 2, "right" } } };
 
-    char direction { '\0' };
-    std::cin >> direction;
+    const Stage jungleWin { 1, "You escaped the jungle! You win!", {} };
 
-    if ('l' == direction)
+    const Stage jungleLose { 2, "You are eaten by a grue. You lose.", {} };
+
+    while (true)
     {
-        std::cout << "You escape and win!" << std::endl;
-    }
-    else
-    {
-        std::cout << "You are eaten by a grue. You lose." << std::endl;
+        std::cout << jungle.description << std::endl;
+
+        if (jungle.choices.empty())
+        {
+            break;
+        }
+
+        for (std::size_t i { 0 }; i < jungle.choices.size(); ++i)
+        {
+            std::cout << "(" << (i + 1) << ") " << jungle.choices.at(i).description << std::endl;
+        }
+
+        std::size_t number { '\0' };
+        std::cin >> number;
+        std::cout << "\n\n";
+
+        const std::size_t nextStageId { jungle.changeStage(number) };
+        std::cout << "Transition to stage " << nextStageId << std::endl;
     }
 
     return EXIT_SUCCESS;
 }
+
+/*
+ *
+ * You are int eh jungle.
+ * (1) "to go left"
+ * (2) "to go right"
+ * */
