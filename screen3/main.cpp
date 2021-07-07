9244207465b38c91a2c77def07145e4b0f3775f8
Ziesche Til Newman <tilnewman@protonmail.com>
2021-07-07 09:06:01 -0600

    cycling rotation and colors

--- screen3/main.cpp
+++ screen3/main.cpp
@@ -10,22 +10,19 @@
 #include <SFML/Graphics.hpp>
 
 //
-float randomBgColorSpeed(const util::Random & random) { return random.fromTo(0.1f, 1.5f); }
-float randomTextSpeed(const util::Random & random) { return random.fromTo(0.5f, 1.75f); }
-float randomLineSpeed(const util::Random & random) { return random.fromTo(0.1f, 2.0f); }
-float randomLineColorSpeed(const util::Random & random) { return random.fromTo(0.1f, 1.0f); }
-float randomChangeTime(const util::Random & random) { return random.fromTo(10.0f, 20.0f); };
-
-//
 struct Context
 {
     Context()
         : random()
-        , window(sf::VideoMode(1920, 1080), "Screen", sf::Style::Fullscreen)
+        , window(sf::VideoMode(1920, 1080), "Screensaver", sf::Style::Fullscreen)
         , bloomWindow(window)
+        , windowSize(static_cast<float>(window.getSize().x), static_cast<float>(window.getSize().y))
+        , windowRect({}, windowSize)
         , frameTimeSec(0.0f)
         , bgTexture()
         , bgSprite()
+        , swirlTexture()
+        , swirlSprite()
     {
         window.setFramerateLimit(60);
 
@@ -37,6 +34,7 @@ struct Context
             window.close();
         }
 
+        // backgrond image
         bgTexture.loadFromFile("image/wood.jpg");
         bgTexture.setSmooth(true);
         bgTexture.setRepeated(true);
@@ -44,67 +42,192 @@ struct Context
         bgSprite.setTexture(bgTexture);
         bgSprite.setTextureRect(sf::IntRect(0, 0, window.getSize().x, window.getSize().y));
         bgSprite.setScale(2.0f, 2.0f);
+
+        // swirl image
+        swirlTexture.loadFromFile("image/swirl-1.png");
+        swirlTexture.setSmooth(true);
+
+        swirlSprite.setTexture(swirlTexture);
+        swirlSprite.setScale(3.0f, 3.0f);
+        util::setOriginToCenter(swirlSprite);
+        swirlSprite.setPosition(util::center(windowRect));
     }
 
     util::Random random;
     sf::RenderWindow window;
     util::BloomEffectHelper bloomWindow;
+    sf::Vector2f windowSize;
+    sf::FloatRect windowRect;
 
     float frameTimeSec;
 
     sf::Texture bgTexture;
     sf::Sprite bgSprite;
+
+    sf::Texture swirlTexture;
+    sf::Sprite swirlSprite;
 };
 
 //
-sf::Vector2f randomWindowPos(const Context & context)
-{
-    return { static_cast<float>(context.random.zeroTo(context.window.getSize().x)),
-             static_cast<float>(context.random.zeroTo(context.window.getSize().y)) };
-}
-
-//
-class PositionCycler : public util::PosSlider
+template <typename T = float>
+class ValueDrifter
 {
   public:
-    PositionCycler(const sf::Vector2f & FROM, const sf::Vector2f & TO, const float SPEED)
-        : PosSlider(FROM, TO, SPEED, util::WillOscillate::No, util::WillAutoStart::Yes, 0)
+    ValueDrifter()
+        : valueRange(0.0f, 0.0f)
+        , speedRange(0.0f, 0.0f)
+        , slider()
     {}
 
-    virtual ~PositionCycler() = default;
-
-    void updateAndLoopIfStopped(
-        const float FRAME_TIME_SEC, const sf::Vector2f & TO, const float SPEED)
+    ValueDrifter(
+        const Context & context,
+        const sf::Vector2<T> & VALUE_RANGE,
+        const sf::Vector2<T> & SPEED_RANGE)
+        : valueRange(VALUE_RANGE)
+        , speedRange(SPEED_RANGE)
+        , slider()
     {
-        if (!updateAndReturnIsMoving(FRAME_TIME_SEC))
+        reset(context);
+    }
+
+    void update(const Context & context)
+    {
+        slider.update(context.frameTimeSec);
+
+        if (slider.isStopped())
         {
-            from_ = to_;
-            to_ = TO;
-            speed(SPEED);
-            resetFrom();
+            reset(context);
         }
     }
+
+    float value() { return slider.value(); }
+
+    void reset(const Context & context)
+    {
+        slider = util::SliderFromTo<float>(
+            slider.value(),
+            context.random.fromTo(valueRange.x, valueRange.y),
+            context.random.fromTo(speedRange.x, speedRange.y));
+    }
+
+  private:
+    sf::Vector2<T> valueRange;
+    sf::Vector2<T> speedRange;
+    util::SliderFromTo<T> slider;
 };
 
-class ColorCycler : public util::ColorSlider
+//
+class PositionDrifter
 {
   public:
-    ColorCycler(const sf::Color & FROM, const sf::Color & TO, const float SPEED)
-        : ColorSlider(FROM, TO, SPEED, util::WillOscillate::No, util::WillAutoStart::Yes, 0)
+    PositionDrifter()
+        : valueRange(0.0f, 0.0f, 0.0f, 0.0f)
+        , speedRange(0.0f, 0.0f)
+        , horizDrifter()
+        , vertDrifter()
     {}
 
-    virtual ~ColorCycler() = default;
-
-    void updateAndLoopIfStopped(const float FRAME_TIME_SEC, const sf::Color & TO, const float SPEED)
+    PositionDrifter(
+        const Context & context,
+        const sf::FloatRect & VALUE_RANGE,
+        const sf::Vector2f & SPEED_RANGE)
+        : valueRange(VALUE_RANGE)
+        , speedRange(SPEED_RANGE)
+        , horizDrifter()
+        , vertDrifter()
     {
-        if (!updateAndReturnIsMoving(FRAME_TIME_SEC))
+        reset(context);
+    }
+
+    void update(const Context & context)
+    {
+        horizDrifter.update(context);
+        vertDrifter.update(context);
+    }
+
+    sf::Vector2f value() { return { horizDrifter.value(), vertDrifter.value() }; }
+
+    void reset(const Context & context)
+    {
+        horizDrifter =
+            ValueDrifter<float>(context, { valueRange.left, util::right(valueRange) }, speedRange);
+
+        vertDrifter =
+            ValueDrifter<float>(context, { valueRange.top, util::bottom(valueRange) }, speedRange);
+    }
+
+  private:
+    sf::FloatRect valueRange;
+    sf::Vector2f speedRange;
+    ValueDrifter<float> horizDrifter;
+    ValueDrifter<float> vertDrifter;
+};
+
+//
+class ColorDrifter
+{
+  public:
+    ColorDrifter()
+        : from_(sf::Color::White)
+        , to_(sf::Color::White)
+        , current_(sf::Color::White)
+        , speedRange_(0.0f, 0.0f)
+        , slider_()
+    {}
+
+    ColorDrifter(const Context & context, const sf::Vector2f & SPEED_RANGE)
+        : from_(sf::Color::White)
+        , to_(sf::Color::White)
+        , current_(sf::Color::White)
+        , speedRange_(SPEED_RANGE)
+        , slider_()
+    {
+        reset(context);
+    }
+
+    void update(const Context & context)
+    {
+        slider_.update(context.frameTimeSec);
+
+        const auto RATIO{ slider_.value() };
+
+        const auto RED_DIFF{ static_cast<float>(to_.r - from_.r) };
+        const auto GREEN_DIFF{ static_cast<float>(to_.g - from_.g) };
+        const auto BLUE_DIFF{ static_cast<float>(to_.b - from_.b) };
+        const auto ALPHA_DIFF{ static_cast<float>(to_.a - from_.a) };
+
+        const auto RED{ static_cast<float>(from_.r) + (RED_DIFF * RATIO) };
+        const auto GREEN{ static_cast<float>(from_.g) + (GREEN_DIFF * RATIO) };
+        const auto BLUE{ static_cast<float>(from_.b) + (BLUE_DIFF * RATIO) };
+        const auto ALPHA{ static_cast<float>(from_.a) + (ALPHA_DIFF * RATIO) };
+
+        current_ = sf::Color(
+            static_cast<sf::Uint8>(RED),
+            static_cast<sf::Uint8>(GREEN),
+            static_cast<sf::Uint8>(BLUE),
+            static_cast<sf::Uint8>(ALPHA));
+
+        if (slider_.isStopped())
         {
-            from_ = to_;
-            to_ = TO;
-            speed(SPEED);
-            resetFrom();
+            reset(context);
         }
     }
+
+    sf::Color value() { return current_; }
+
+    void reset(const Context & context)
+    {
+        from_ = to_;
+        to_ = colors::randomVibrant(context.random);
+        slider_ = util::SliderZeroToOne(context.random.fromTo(speedRange_.x, speedRange_.y));
+    }
+
+  private:
+    sf::Color from_;
+    sf::Color to_;
+    sf::Color current_;
+    sf::Vector2f speedRange_;
+    util::SliderZeroToOne slider_;
 };
 
 //
@@ -115,6 +238,10 @@ int main()
 {
     Context context;
 
+    PositionDrifter positionDrifter(context, context.windowRect, { 0.15f, 0.65f });
+    ValueDrifter rotateSpeedDrifter(context, { -30.0f, -100.0f }, { 0.5f, 2.0f });
+    ColorDrifter colorDrifter(context, { 0.1f, 1.0f });
+
     sf::Clock frameClock;
 
     while (context.window.isOpen())
@@ -123,9 +250,21 @@ int main()
 
         HandleEvents(context);
 
+        // update
+        rotateSpeedDrifter.update(context);
+        context.swirlSprite.rotate(rotateSpeedDrifter.value());
+
+        positionDrifter.update(context);
+        context.swirlSprite.setPosition(positionDrifter.value());
+
+        colorDrifter.update(context);
+        context.swirlSprite.setColor(colorDrifter.value());
+
         // draw
         context.bloomWindow.clear();
         context.bloomWindow.draw(context.bgSprite);
+        context.bloomWindow.draw(context.swirlSprite);
+        context.bloomWindow.draw(context.swirlSprite, sf::BlendAdd);
         context.bloomWindow.display();
     }
 
